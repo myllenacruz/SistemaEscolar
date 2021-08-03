45a372c3fe8ae2f21702a0e525d7ec37af1ea200
Myllena Cruz <myllena12345@hotmail.com>
2021-08-03 16:07:16 -0300

    Organização de rotas

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
+const routes = require('./routes/index')
 
-app.use(express.json())
+const app = express()
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/test', (req, res) => res.status(200).send({ message: 'Ok :}' }))
+routes(app)
 
 app.listen(3000, () => {
   console.log('Ok!')
