9c79e49c57472d3b033e23234dd0697145a6eb23
Myllena Cruz <myllena12345@hotmail.com>
2021-08-03 18:35:21 -0300

    Atualizando e deletando registros

--- app/controllers/PeopleController.js
+++ app/controllers/PeopleController.js
@@ -31,6 +31,30 @@ class PeopleController {
       return res.status(500).json(error.message)
     }
   }
+
+  static async updatePerson(req, res) {
+    const { id } = req.params
+    const newInfo = req.body
+    try {
+      await database.People.update(newInfo, { where: { id: Number(id) } })
+      const updatedPerson = await database.People.findOne({
+        where: { id: Number(id) },
+      })
+      return res.status(200).json(updatedPerson)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async deletePerson(req, res) {
+    const { id } = req.params
+    try {
+      await database.People.destroy({ where: { id: Number(id) } })
+      return res.status(200).json({ message: 'Ok!' })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
 module.exports = PeopleController
--- app/routes/peopleRoute.js
+++ app/routes/peopleRoute.js
@@ -5,5 +5,7 @@ const router = Router()
 router.get('/people', PeopleController.catchPeople)
 router.get('/people/:id', PeopleController.catchPerson)
 router.post('/people', PeopleController.createPerson)
+router.put('/people/:id', PeopleController.updatePerson)
+router.delete('/people/:id', PeopleController.deletePerson)
 
 module.exports = router
